4bd80acacac8f386e427f3fdb312eb18447f0ce7
Ivan Vivas Garcia <ivg77850@lasallebajio.edu.mx>
2024-10-02 11:25:57 -0600

    Optimizacion de codigo

--- Semestre_05/ModeladoYProcesamientoDeImagenes/Segundo Parcial/src/ecualizacion.py
+++ Semestre_05/ModeladoYProcesamientoDeImagenes/Segundo Parcial/src/ecualizacion.py
@@ -1,20 +1,18 @@
 import numpy as np
 import utils
 
-#Función de ecualización de histograma usando la LUT
-def histogram_equalization(img):
+def ecualizacion_histograma(img):
     total_pixeles = img.size
-    hist = utils.histograma(img, 256, False)
-    lookup_table = utils.crear_lookup_table(hist, total_pixeles)
+    histograma = utils.histograma(img, 256, False)
+    LUT = utils.crear_lookup_table(histograma, total_pixeles)
 
-    #Aplicar la LUT a la imagen
-    img_eq = np.zeros_like(img)
+    img_ecualizada = np.zeros_like(img)
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
             pixel = img[i, j]
-            img_eq[i, j] = lookup_table[pixel][5]
+            img_ecualizada[i, j] = LUT[pixel][5]
 
-    return img_eq
+    return img_ecualizada
 
 def imprimir_imagenes(img_original):
     img_cuantizada = utils.cuantizar(img_original, 8, 3)
@@ -22,13 +20,13 @@ def imprimir_imagenes(img_original):
     histograma_img_original = utils.histograma(img_original, 256, False)
     histograma_img_cuantizada = utils.histograma(img_cuantizada, 256, False)
 
-    img_original_ecualizada = histogram_equalization(img_original)
-    img_cuantizada_ecualizada = histogram_equalization(img_cuantizada)
+    img_original_ecualizada = ecualizacion_histograma(img_original)
+    img_cuantizada_ecualizada = ecualizacion_histograma(img_cuantizada)
 
     histograma_original_ecualizada = utils.histograma(img_original_ecualizada, 256, False)
     histograma_cuantizada_ecualizada = utils.histograma(img_cuantizada_ecualizada, 256, False)
 
-        # LISTAS DE IMAGENES E HISTOGRAMAS
+    # LISTAS DE IMAGENES E HISTOGRAMAS
     lista_pre = [img_original, img_cuantizada]
     lista_titulos_pre = ['Imagen Original', 'Imagen Cuantizada']
     lista_histograma_pre = [histograma_img_original, histograma_img_cuantizada]
