1e3a9fa27b60977ba0e0afb5e631568aeaeaa451
Ivan Vivas Garcia <ivg77850@lasallebajio.edu.mx>
2024-10-01 19:03:10 -0600

    Commit General

---
+++ Semestre_05/DesarrolloParaPlataformaAndroid/LaSalleApp/app/src/main/java/com/example/lasalleapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.lasalleapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.lasalleapp.ui.theme.LaSalleAppTheme
+import com.example.lasalleapp.ui.theme.screens.HomeScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LaSalleAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()){ innerPadding ->
+                    HomeScreen(innerPadding = innerPadding)
+                }
+            }
+        }
+    }
+}
