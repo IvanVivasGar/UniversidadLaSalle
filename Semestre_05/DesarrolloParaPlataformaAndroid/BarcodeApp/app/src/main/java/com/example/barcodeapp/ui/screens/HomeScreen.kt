6c6dce5e32f63ae851ae1d13d07706a183a11f12
Ivan Vivas Garcia <ivg77850@lasallebajio.edu.mx>
2024-10-29 20:37:09 -0600

    Modificaciones HomeScreen
    Eliminacion de importaciones no necesarias

--- Semestre_05/DesarrolloParaPlataformaAndroid/BarcodeApp/app/src/main/java/com/example/barcodeapp/ui/screens/HomeScreen.kt
+++ Semestre_05/DesarrolloParaPlataformaAndroid/BarcodeApp/app/src/main/java/com/example/barcodeapp/ui/screens/HomeScreen.kt
@@ -6,11 +6,9 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
@@ -26,7 +24,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
 import com.example.barcodeapp.models.Product
 import com.example.barcodeapp.ui.components.ProductCard
 import com.example.barcodeapp.ui.theme.BarcodeAppTheme
