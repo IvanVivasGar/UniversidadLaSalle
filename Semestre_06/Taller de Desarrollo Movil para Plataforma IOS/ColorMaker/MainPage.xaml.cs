c7058321cc5905f01452afc782afd15972b251ac
Ivan Vivas Garcia <ivg77850@lasallebajio.edu.mx>
2025-03-10 19:39:12 +0100

    ColorMaker

---
+++ Semestre_06/Taller de Desarrollo Movil para Plataforma IOS/ColorMaker/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using CommunityToolkit.Maui.Core;
+namespace ColorMaker;
+public partial class MainPage : ContentPage
+{
+	bool isRandom;
+	string hexValue = string.Empty; // Initialize with an empty string
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+	{
+		if(!isRandom)
+		{
+			var red = sldRed.Value;
+			var green = sldGreen.Value;
+			var blue = sldBlue.Value;
+
+			Color color = Color.FromRgb(red, green, blue);
+
+			SetColor(color);
+		}
+	}
+
+	private void SetColor(Color color)
+	{
+		Debug.WriteLine(color.ToString());
+		hexValue = color.ToHex();
+		lblHex.Text = hexValue;
+		colorPreview.BackgroundColor = color;
+	}
+
+	private async void btnRandom_Clicked(object sender, EventArgs e)
+	{
+		isRandom = true;
+		var random = new Random();
+
+		var color = Color.FromRgb(
+			random.Next(0,256),
+			random.Next(0, 256),
+			random.Next(0, 256));
+
+		SetColor(color);
+
+		sldRed.Value = color.Red;
+		sldGreen.Value = color.Green;
+		sldBlue.Value = color.Blue;
+		isRandom = false;
+
+		// Added clipboard functionality from ImageButton_Clicked
+		await Clipboard.SetTextAsync(hexValue);
+		var toast = CommunityToolkit.Maui.Alerts.Toast.Make("Color copied",
+			CommunityToolkit.Maui.Core.ToastDuration.Short,
+			12);
+		await toast.Show();
+	}
+}
---
+++ Semestre_06/Taller de Desarrollo Movil para Plataforma IOS/ColorMaker/obj/Debug/net9.0-maccatalyst/maccatalyst-arm64/nativelibraries/aot-output/arm64/aot-instances.dll.s
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4f8cda305988a040aa989d94640e91bf473b36868720787e3aed5560e586d0db
+size 154751349
